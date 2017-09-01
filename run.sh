c2ed67b76cac371bf461c1e9328a52d7d165681e
bufanda <bufanda@gmx.de>
2017-09-02 00:08:18 +0200

    run.sh: restructured run and add environment variables for steam auth keys

--- run.sh
+++ run.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-DATA=/home/docker/volumes/garrysmod
-
+CONFIG="-v ./configs/server.cfg:/opt/steam/garrysmod/cfg/server.cfg:ro"
+AUTOEXEC="-v ./configs/autoexec.cfg:/opt/steam/garrysmod/cfg/autoexec.cfg:ro"
+PORTS="-p 27015:27015 -p 27015:27015/udp -p 27005:27005/udp -p 27020:27020/udp -p 26900:26900/udp"
+NAME=terrortown
+ENV="-e ENV_DOCKER_WORKSHOP_COLLECTION= -e ENV_DOCKER_STEAM_ACC_KEY=  -e ENV_DOCKER_STEAM_AUTH_KEY="
+ENVPORT="-e ENV_DOCKER_TTT_PORT=27015"
 
 case "$1" in
 	start)
-		if [ -z "`docker ps -a | grep garrysmod`" ]; then
-			docker run --name garrysmod -d -p 27015:27015 -p 27015:27015/udp -p 27005:27005/udp -p 27020:27020/udp -p 26900:26900/udp -v ${DATA}:/root/mounted gmserver
+		if [ -z "`docker ps -a | grep ${NAME}`" ]; then
+			docker run --name ${NAME} -d ${PORTS} ${ENV} ${ENVPORT} ${AUTOEXEC} ${CONFIG} bufanda/terrortown
 		else
-			docker start garrysmod
+			docker start ${NAME}
 		fi
 	;;
 	stop)
-		docker stop garrysmod
+		docker stop ${NAME}
 	;;
 	restart)
-		docker restart garrysmod
+		docker restart ${NAME}
 	;;
 	status)
-		docker ps | grep garrysmod
+		docker ps | grep ${NAME}
 	;;
 	*)
 		echo "Available options are [start|stop|restart|status]"
 	;;
 esac
-
-
